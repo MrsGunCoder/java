1c53168eb02483e7e5f0e885ed95f18f47d3f210
MrsGunCoder <ghacmmmdova@mail.ru &    git config --global user.password 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 &    git config --global credential.helper store &    git config --list --show-origin &     git pullgit config --global credential.helper store &    git config --global user.email ghacmmmdova@mail.ru>
2024-04-30 23:27:45 +0100

    i added files

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+var ad = prompt("Zehmet olmasa adinizi yazin");
+var soyad = prompt("Zehmet olmasa soyadinizi yazin");
+var il = prompt("Zehmet olmasa dogum ilinizi yazin");
+
+var yas = 2024 - il;
+
+alert(ad + " senin yasin  " + yas);
